18e272592a4fc1b6780a55b28bb7cde8862e4dd0
Deepinder Singh <Deepinder999@gmail.com>
2022-03-31 12:11:14 +0530

    add two accounts into git

--- 25. vue.js/resto-project/src/App.vue
+++ 25. vue.js/resto-project/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class="logo" alt="Vue logo" src="./assets/resto.webp">
+  <SignUp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUp
   }
 }
 </script>
@@ -23,4 +23,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.logo{
+  width: 200px;
+}
+
 </style>
---
+++ 25. vue.js/resto-project/src/components/SignUp.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="sign-up">
+        <h1>Sign Up</h1>
+        <div class="register">
+            <input type="text" name="" id="" placeholder="Enter Name"><br/>
+            <input type="text" name="" id="" placeholder="Enter Email"><br/>
+            <input type="password" name="" id="" placeholder="Enter Password"><br/>
+            <button >Sign Up</button>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+    name : 'SignUp'
+}
+</script>
+<style scoped>
+.register input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid skyblue;
+}
+.register button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    color: #fff;
+    background-color: skyblue;
+    cursor: pointer;
+}
+</style>
