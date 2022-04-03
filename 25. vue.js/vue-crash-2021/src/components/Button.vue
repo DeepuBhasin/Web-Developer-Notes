e969f4db63b022f67abff309be54aa3911bd1237
Deepinder Singh <Deepinder999@gmail.com>
2022-04-03 19:10:30 +0530

    Working on vue js

---
+++ 25. vue.js/vue-crash-2021/src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button @click="onClick()" class="btn" :style="{background:color}">{{text}}</button>
+</template>
+<script>
+export default {
+    name : 'Button',
+    props : {
+        text : String,
+        color : String,
+    }
+}
+
+</script>
+<style>
+
+</style>
---
+++ 25. vue.js/vue-crash-2021/src/components/TaskComponent.vue
@@ -0,0 +1,18 @@
+<template>
+   <div>
+        <div v-for="item in tasks" :key="item.id">
+        <h3>{{item.text}}</h3>
+    </div>
+   </div>
+</template>
+<script>
+export default {
+    name :  'TaskComponent',
+    props:{
+        tasks : Array
+    }    
+}
+</script>
+<style scoped>
+
+</style>
